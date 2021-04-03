558251b8a81b128d40d77c5b52bc4d4cba2e21fd
Ariful4593 <arifkhagoria2015@gmail.com>
2021-04-03 19:59:41 +0600

    Linked list code updated

--- remove_node_from_linked_list.c
+++ remove_node_from_linked_list.c
@@ -4,12 +4,13 @@ struct node{
     int data;
     Node *next;
 };
+
 Node *create_node(int item, Node *next)
 {
     Node *new_node = (Node *) malloc(sizeof(Node));
     if(new_node == NULL){
-        printf("Error! Could not create new node\n");
-        return;
+        printf("Error! Could Not Create A New Node\n");
+        exit(1);
     }
     new_node->data = item;
     new_node->next = next;
@@ -47,24 +48,10 @@ Node *remove_node(Node *head, Node *node)
     return head;
 }
 
-Node *append(Node *head, int item)
-{
-    Node *new_node = create_node(item, head);
-    if(head == NULL){
-        return new_node;
-    }
-    Node *current_node = head;
-    while(current_node->next != NULL){
-        current_node = current_node->next;
-    }
-    current_node->next = new_node;
-    return head;
-}
-
 void print_linked_list(Node *head)
 {
     Node *current_node = head;
-    while(current_node == NULL){
+    while(current_node != NULL){
         printf("%d ", current_node->data);
         current_node = current_node->next;
     }
@@ -72,14 +59,15 @@ void print_linked_list(Node *head)
 }
 int main()
 {
-    Node *n1,*n2,*head;
+    Node *n1, *n2, *n3, *head;
     n1 = create_node(10, NULL);
+
     head = n1;
     print_linked_list(head);
     head = prepend(head, 20);
     print_linked_list(head);
 
-    head = append(head, 30);
+    head = prepend(head, 30);
     print_linked_list(head);
 
     head = remove_node(head, n1);
@@ -91,3 +79,11 @@ int main()
 
     return 0;
 }
+/*
+    Sample Output:
+    10
+    20 10
+    30 20 10
+    30 20
+    20
+*/
